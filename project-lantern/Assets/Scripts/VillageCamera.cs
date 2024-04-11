d8aab4d6fa9a2bedf66324707a35c867efe4f4f4
Juliakp1 <juliakp710@gmail.com>
2024-04-11 12:56:15 -0300

    fixed issues + more assets

--- project-lantern/Assets/Scripts/VillageCamera.cs
+++ project-lantern/Assets/Scripts/VillageCamera.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class VillageCamera : MonoBehaviour
 {
     [SerializeField] private Transform movePoint;
     [SerializeField] private float speed = 3;
     
-    public float targetSize;
+    public float targetSize = 8;
 
     void Start() {
         movePoint.parent = null; // Detach partent
@@ -16,6 +17,7 @@ public class VillageCamera : MonoBehaviour
     void Update() {
         float movementAmout = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, movementAmout);
-        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, targetSize, 0.4f);
+        float blend = 1f - Mathf.Pow(1f - 0.1f, Time.deltaTime * 30);
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, targetSize, blend);
     }
 }
