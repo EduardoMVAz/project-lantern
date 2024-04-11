20b4768694a9600a1ec172041b0389136346156a
P-ASilva <pedroas5.al.insper.edu>
2024-04-11 12:56:21 -0300

    feat: keyboard and controller navigation

--- project-lantern/Assets/Scripts/MenuController.cs
+++ project-lantern/Assets/Scripts/MenuController.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.EventSystems;
 public class MenuController : MonoBehaviour
 {
+    public GameObject settingsMenu;
+    public GameObject settingsFirst, settingsClose, mainFirst, creditsFirst, creditsClose;
     [SerializeField] private string start_scene;
     public void Start()
     {
@@ -10,5 +12,16 @@ public class MenuController : MonoBehaviour
     public void Play()
     {   
         SceneManager.LoadScene(start_scene);
+    }
+
+    public void OpenSettings() {
+        settingsMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(settingsFirst);
+    }
+    public void CloseSettings() {
+        settingsMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(settingsClose);
     }
 }
