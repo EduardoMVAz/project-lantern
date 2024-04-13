3f256143a62039b9cf086f95f6d0fe76f55b41c7
EduardoMVAz <emendesvaz@gmail.com>
2024-04-13 17:45:45 -0300

    FEATURES: RoughGround, initialLighting, remainingLight features added and 'tested'

--- project-lantern/Assets/Scripts/LevelManager.cs
+++ project-lantern/Assets/Scripts/LevelManager.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class LevelManager : MonoBehaviour
 {
     
     [SerializeField] private int moveAmount;
+    [SerializeField] private Light2D initialLight;
+    private float initialLightCountdown = 3.0f;
+    private bool canMove = false;
 
     public int GetMoveAmount() {
         return moveAmount;
     }
 
+    public bool GetCanMove() {
+        return canMove;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        initialLightCountdown -= Time.deltaTime;
+        if (initialLightCountdown <= 0) {
+            initialLight.intensity = 0.0f;
+            canMove = true;
+        }
     }
 }
--- project-lantern/Assets/Scripts/PlayerController.cs
+++ project-lantern/Assets/Scripts/PlayerController.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using UnityEngine.Experimental.Rendering.Universal;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using TMPro;
 
 public class PlayerController : MonoBehaviour {
 
     public bool inMenu;
 
+    [SerializeField] private GameObject levelManager;
+
+    [SerializeField] private TextMeshProUGUI remainingLightText;
     [SerializeField] private float speed = 3;
     [SerializeField] private Transform movePoint;
     [SerializeField] private LayerMask obstacleMask;
 
     private Light2D lighty;
     private Animator anim;
+
+    private bool canMove = false;
     private bool walked = false;
 
     private int moveAmount;
     private int moveAmountMax;
+    private int moveCost = 1;
     private int lightInnerRadiusDefault;
     private int lightOuterRadiusDefault;
 
@@ -27,29 +34,34 @@ public class PlayerController : MonoBehaviour {
         lighty = GetComponent<Light2D>(); // control the light like this!
         inMenu = false;
 
-        moveAmountMax = GameObject.Find("LevelManager").GetComponent<LevelManager>().GetMoveAmount();
+        moveAmountMax = levelManager.GetComponent<LevelManager>().GetMoveAmount();
         moveAmount = moveAmountMax;
+        SetRemainingLightText();
+        
         lightInnerRadiusDefault = (int)lighty.pointLightInnerRadius;
         lightOuterRadiusDefault = (int)lighty.pointLightOuterRadius;
     }
 
     void Update() {
-        float movementAmout = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, movementAmout);
 
-        if (!inMenu) {
+        if (!canMove) {
+            canMove = levelManager.GetComponent<LevelManager>().GetCanMove();
+        }
+
+        float movementAmount = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, movementAmount);
+
+        if (!inMenu && canMove) {
             if (Vector3.Distance(transform.position, movePoint.position) == 0.0f) {
                 if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) >= 0.9f) {
                     if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, obstacleMask) && walked == false) {
                         Move(new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0));
                         walked = true;
-                        lighty.color = Color.red;
                     }
                 } else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) >= 0.9f) {
                     if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, obstacleMask) && walked==false) {
                         Move(new Vector3(0, Input.GetAxisRaw("Vertical"), 0));
                         walked = true;
-                        lighty.color = Color.red;
                     }
                 }
                 anim.SetBool("moving", false);
@@ -59,7 +71,6 @@ public class PlayerController : MonoBehaviour {
 
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) < 0.5f) {
                 walked = false;
-                lighty.color = Color.yellow;
             }
 
         } // outside here would be while in-menu
@@ -69,15 +80,35 @@ public class PlayerController : MonoBehaviour {
         Vector3 newPosition = movePoint.position + direction;
         if (!Physics2D.OverlapCircle(newPosition, 0.2f, obstacleMask)) {
             movePoint.position = newPosition;
-            moveAmount--;
+            moveAmount -= moveCost;
 
             // Update the light
-            UpdateLight();
+            if (moveAmount >= 0) {
+                UpdateLight();
+            }
+            SetRemainingLightText();
+        }
+    }
+
+    // detect collision with roughGround tag
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag.Equals("roughGround")) {
+            moveCost = 2;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag.Equals("roughGround")) {
+            moveCost = 1;
         }
     }
 
     private void UpdateLight() {
         lighty.pointLightInnerRadius = lightInnerRadiusDefault * moveAmount/moveAmountMax;
         lighty.pointLightOuterRadius = lightOuterRadiusDefault * moveAmount/moveAmountMax;
+    }
+
+    private void SetRemainingLightText() {
+        remainingLightText.text = "Remaining Light: " + moveAmount + " / " + moveAmountMax;
     }
 }
