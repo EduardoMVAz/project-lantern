31521f56fe5d94c3d1fa7b18daf34f4988bcf972
Juliakp1 <juliakp710@gmail.com>
2024-04-17 11:13:25 -0300

    questGiver dialogue + level fly-lights

--- project-lantern/Assets/Scripts/LS_iconManager.cs
+++ project-lantern/Assets/Scripts/LS_iconManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,22 +6,27 @@ using UnityEngine.UI;
 
 public class LS_iconManager : MonoBehaviour {
 
-    public GameObject level0, level1, level2, level3, level4, level5, level6, level7, level8, level9, level10;
+    public GameObject[] levelButtons;
+    public GameObject[] levelFlies;
     public Sprite[] sprites;
 
     void Start() {
         
-        level0.GetComponent<Image>().sprite = sprites[PlayerPrefs.GetInt("Level 0", 0)];
-        level1.GetComponent<Image>().sprite = sprites[PlayerPrefs.GetInt("Level 1", 0)];
-        level2.GetComponent<Image>().sprite = sprites[PlayerPrefs.GetInt("Level 2", 0)];
-        level3.GetComponent<Image>().sprite = sprites[PlayerPrefs.GetInt("Level 3", 0)];
-        level4.GetComponent<Image>().sprite = sprites[PlayerPrefs.GetInt("Level 4", 0)];
-        level5.GetComponent<Image>().sprite = sprites[PlayerPrefs.GetInt("Level 5", 0)];
-        level6.GetComponent<Image>().sprite = sprites[PlayerPrefs.GetInt("Level 6", 0)];
-        level7.GetComponent<Image>().sprite = sprites[PlayerPrefs.GetInt("Level 7", 0)];
-        level8.GetComponent<Image>().sprite = sprites[PlayerPrefs.GetInt("Level 8", 0)];
-        level9.GetComponent<Image>().sprite = sprites[PlayerPrefs.GetInt("Level 9", 0)];
-        level10.GetComponent<Image>().sprite = sprites[PlayerPrefs.GetInt("Level 10", 0)];
-        
+        for (int i = 0; i < levelButtons.Length; i++) {
+            int index = PlayerPrefs.GetInt("Level " + i.ToString(), 0);
+            levelButtons[i].GetComponent<Image>().sprite = sprites[index];
+            levelFlies[i].SetActive(false);
+        }
+        StartCoroutine(ActivateLights());
+    }
+
+    IEnumerator ActivateLights() {
+        for (int i = 0; i < levelFlies.Length; i++) {
+            int index = PlayerPrefs.GetInt("Level " + i.ToString(), 0);
+            if (index != 0) {
+                levelFlies[i].SetActive(true);
+                yield return new WaitForSeconds(1f);
+            }
+        }
     }
 }
