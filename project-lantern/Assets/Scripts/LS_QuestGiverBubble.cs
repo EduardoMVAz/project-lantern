d6d5e8c7d22fa92a23a08dab896f361c4abceae1
P-ASilva <pedroas5.al.insper.edu>
2024-06-24 23:17:57 -0300

    logic rewrite

--- project-lantern/Assets/Scripts/LS_QuestGiverBubble.cs
+++ project-lantern/Assets/Scripts/LS_QuestGiverBubble.cs
@@ -6,44 +6,24 @@ using UnityEngine.EventSystems;
 public class LS_questGiverBubble : MonoBehaviour
 {
     [SerializeField] private float timerLimit;
-    [SerializeField] private GameObject[] bubbles;
+    [SerializeField] private GameObject bubble;
     private bool playerInside = false;
     private int index = 0;
     private int lastIndex;
     private float timer;
 
     void Start() {
-        for (int i = 0; i < bubbles.Length; i++) {
-            bubbles[i].SetActive(false); 
-        }
         timer = timerLimit;
-        lastIndex = bubbles.Length-1;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (playerInside) {
-            timer -=  Time.deltaTime * 5;
-            if (timer <= 0.0f) {
-                timer = timerLimit;
-
-                // moves to the next text bubble
-                index++;
-                if (index >= bubbles.Length) {
-                    index = 0;
-                }
-                lastIndex++;
-                if (lastIndex >= bubbles.Length) {
-                    lastIndex = 0;
-                }
-            }
-            // switches bubble
-            bubbles[index].SetActive(true);
-            bubbles[lastIndex].SetActive(false);
+            bubble.SetActive(true);
 
         } else {
-            bubbles[index].SetActive(false);
+            bubble.SetActive(false);
         }
     }
 
